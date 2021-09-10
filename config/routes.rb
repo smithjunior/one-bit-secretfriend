b9c587297efe9fa988f85a006e2b5760ef9547f5
Smith Júnior <smith.junior@icloud.com>
2021-09-09 22:42:46 -0300

    Added new files to create tests

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: "registrations" }
   mount Sidekiq::Web => "/sidekiq"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+  resources :campaigns, except: [:new] do
+    post 'raffle', on: :member
+    # post 'raffle', on: :collection
+  end
+  get 'members/:token/opened', to: 'members#opened'
+  resources :members, only: [:create, :destroy, :update]
 end
